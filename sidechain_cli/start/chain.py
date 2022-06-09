f10fe884267ac6151bbfc42aa5782ad551536b06
Mayukha Vadari <mvadari@ripple.com>
2022-06-09 12:46:13 -0400

    feat: implement CLI method to start rippled standalone (#4)
    * implement start_chain
    
    * switch to options instead of arguments

--- sidechain_cli/start/chain.py
+++ sidechain_cli/start/chain.py
@@ -1,18 +1,44 @@
 """CLI function for starting a rippled node."""
 
+import os
+import subprocess
+
 import click
 
 
 @click.command(name="chain")
-@click.argument("rippled")
-@click.argument("config")
-def start_chain(rippled: str, config: str) -> None:
+@click.option(
+    "--rippled",
+    required=True,
+    type=click.Path(exists=True),
+    help="The filepath to the rippled node.",
+)
+@click.option(
+    "--config",
+    required=True,
+    type=click.Path(exists=True),
+    help="The filepath to the rippled config file.",
+)
+@click.option(
+    "--verbose", is_flag=True, help="Whether or not to print more verbose information."
+)
+def start_chain(rippled: str, config: str, verbose: bool = False) -> None:
     """
     Start a standalone node of rippled.
+    \f
 
     Args:
         rippled: The filepath to the rippled node.
         config: The filepath to the rippled config file.
-    """
-    print(rippled)
-    print(config)
+        verbose: Whether or not to print more verbose information.
+    """  # noqa: D301
+    to_run = [rippled, "--conf", config, "-a"]
+    if verbose:
+        print("Starting server...")
+    fout = open(os.devnull, "w")
+    process = subprocess.Popen(
+        to_run, stdout=fout, stderr=subprocess.STDOUT, close_fds=True
+    )
+    pid = process.pid
+    if verbose:
+        print(f"started rippled: {rippled} PID: {pid}", flush=True)
