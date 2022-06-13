a39fc8e65de2da7d4499cab73634244783f5c2ce
Mayukha Vadari <mvadari@ripple.com>
2022-06-13 14:30:48 -0400

    feat: implement `list` command for chains (#9)
    * install tabulate
    
    * implement list

--- sidechain_cli/chain/config.py
+++ sidechain_cli/chain/config.py
@@ -1,9 +1,19 @@
 """Config-related rippled commands."""
 
 import click
+from tabulate import tabulate
+
+from sidechain_cli.utils import get_config
 
 
 @click.command(name="list")
 def list_chains() -> None:
     """Get a list of running rippled nodes."""  # noqa: D301
-    print("Insert list of chains here")
+    config = get_config()
+    print(
+        tabulate(
+            config.chains,
+            headers="keys",
+            tablefmt="presto",
+        )
+    )
