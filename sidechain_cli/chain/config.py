8c497b9386dcde947ac5ded478edd4e7f9398738
Mayukha Vadari <mvadari@ripple.com>
2022-06-14 11:34:33 -0400

    implement list

--- sidechain_cli/chain/config.py
+++ sidechain_cli/chain/config.py
@@ -8,7 +8,7 @@ from sidechain_cli.utils import get_config
 
 @click.command(name="list")
 def list_chains() -> None:
-    """Get a list of running rippled nodes."""  # noqa: D301
+    """Get a list of running rippled nodes."""
     config = get_config()
     if len(config.chains) == 0:
         print("No chains running.")
--- sidechain_cli/witness/config.py
+++ sidechain_cli/witness/config.py
@@ -1,9 +1,22 @@
 """Config-related witness commands."""
 
 import click
+from tabulate import tabulate
+
+from sidechain_cli.utils import get_config
 
 
 @click.command(name="list")
 def list_witnesses() -> None:
-    """Get a list of running witness nodes."""  # noqa: D301
-    print("Insert list of witnesses here")
+    """Get a list of running witness nodes."""
+    config = get_config()
+    if len(config.witnesses) == 0:
+        print("No witnesses running.")
+        return
+    print(
+        tabulate(
+            config.witnesses,
+            headers="keys",
+            tablefmt="presto",
+        )
+    )
