1f2f15666509aced20c0ba767754374500e7b7a7
Mayukha Vadari <mvadari@ripple.com>
2022-06-17 17:12:20 -0400

    combine lists

--- sidechain_cli/witness/__init__.py
+++ sidechain_cli/witness/__init__.py
@@ -2,7 +2,6 @@
 
 import click
 
-from sidechain_cli.witness.config import list_witnesses
 from sidechain_cli.witness.request import get_witness_status, request_witness
 from sidechain_cli.witness.start import restart_witness, start_witness, stop_witness
 
@@ -16,8 +15,6 @@ def witness() -> None:
 witness.add_command(start_witness, name="start")
 witness.add_command(stop_witness, name="stop")
 witness.add_command(restart_witness, name="restart")
-
-witness.add_command(list_witnesses, name="list")
 
 witness.add_command(get_witness_status, name="status")
 witness.add_command(request_witness, name="request")
