bd5e434e818b0ecb2f95a074299e53c13c6d3755
Mayukha Vadari <mvadari@ripple.com>
2022-07-06 11:30:24 -0400

    add basic start/stop test

---
+++ tests/test_basic.py
@@ -0,0 +1,21 @@
+import unittest
+
+from click.testing import CliRunner
+
+from sidechain_cli.main import main
+
+
+class TestBasicCreation(unittest.TestCase):
+    def test_startup_close(self):
+        runner = CliRunner()
+        no_chains_list_output = "No chains running.\nNo witnesses running.\n"
+
+        start_result = runner.invoke(main, ["server", "start-all"])
+        assert start_result.exit_code == 0
+        start_list = runner.invoke(main, ["server", "list"])
+        assert start_list.output != no_chains_list_output
+
+        stop_result = runner.invoke(main, ["server", "stop", "--all"])
+        assert stop_result.exit_code == 0
+        stop_list = runner.invoke(main, ["server", "list"])
+        assert stop_list.output == no_chains_list_output
