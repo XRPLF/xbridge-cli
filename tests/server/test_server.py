884798c7498262ba777218893207f92ecb79be70
Mayukha Vadari <mvadari@ripple.com>
2022-09-15 15:39:18 -0500

    make linter happy

--- tests/server/test_server.py
+++ tests/server/test_server.py
@@ -18,20 +18,24 @@ class TestServer(unittest.TestCase):
     def test_list(self):
         server_list = self.runner.invoke(main, ["server", "list"])
         lines = server_list.output.split("\n")
-        print(server_list.output)
         self.assertEqual(lines[0], "Chains:")
         self.assertRegex(
             lines[1],
-            r"^ +name +\| +pid +\| +rippled +\| +config +\| +ws_ip +\| +ws_port +\| +http_ip +\| +http_port *$",
+            r"^ +name +\| +pid +\| +rippled +\| +config +\| +ws_ip +\| +ws_port +\| +"
+            r"http_ip +\| +http_port *$",
         )
         self.assertRegex(lines[2], r"^-+\+-+\+-+\+-+\+-+\+-+\+-+\+-+$")
         self.assertRegex(
             lines[3],
-            r"^ *issuing_chain *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *issuing_chain *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *\| *[0-9\.]+ *\| *[0-9]+"
+            r" *$",
         )
         self.assertRegex(
             lines[4],
-            r"^ *locking_chain *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *locking_chain *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *\| *[0-9\.]+ *\| *[0-9]+"
+            r" *$",
         )
         self.assertEqual(lines[5], "")
 
@@ -43,38 +47,27 @@ class TestServer(unittest.TestCase):
         self.assertRegex(lines[8], r"^-+\+-+\+-+\+-+\+-+\+-+$")
         self.assertRegex(
             lines[9],
-            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
         )
         self.assertRegex(
             lines[10],
-            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
         )
         self.assertRegex(
             lines[11],
-            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
         )
         self.assertRegex(
             lines[12],
-            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
         )
         self.assertRegex(
             lines[13],
-            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
+            r"^ *witness[0-9] *\| *[0-9]+ *\| *[a-zA-Z0-9-_\.\/]+ *\| *"
+            r"[a-zA-Z0-9-_\/\.]+ *\| *[0-9\.]+ *\| *[0-9]+ *$",
         )
         self.assertEqual(lines[14], "")
-
-
-#          name          |   pid | rippled                                           | config                                                              | ws_ip     |   ws_port | http_ip   |   http_port
-# ---------------+-------+---------------------------------------------------+---------------------------------------------------------------------+-----------+-----------+-----------+-------------
-#  issuing_chain | 64225 | /Users/mvadari/Documents/rippled/mybuild2/rippled | /Users/mvadari/Documents/sidechain-config/issuing_chain/rippled.cfg | 127.0.0.1 |      6008 | 127.0.0.1 |        5006
-#  locking_chain | 64226 | /Users/mvadari/Documents/rippled/mybuild2/rippled | /Users/mvadari/Documents/sidechain-config/locking_chain/rippled.cfg | 127.0.0.1 |      6006 | 127.0.0.1 |        5005
-
-
-# Witnesses:
-#  name     |   pid | witnessd                                                           | config                                                          | ip        |   rpc_port
-# ----------+-------+--------------------------------------------------------------------+-----------------------------------------------------------------+-----------+------------
-#  witness3 | 64227 | /Users/mvadari/Documents/xbridge_witness/my_build/xbridge_witnessd | /Users/mvadari/Documents/sidechain-config/witness3/witness.json | 127.0.0.1 |       6013
-#  witness4 | 64228 | /Users/mvadari/Documents/xbridge_witness/my_build/xbridge_witnessd | /Users/mvadari/Documents/sidechain-config/witness4/witness.json | 127.0.0.1 |       6014
-#  witness2 | 64229 | /Users/mvadari/Documents/xbridge_witness/my_build/xbridge_witnessd | /Users/mvadari/Documents/sidechain-config/witness2/witness.json | 127.0.0.1 |       6012
-#  witness0 | 64230 | /Users/mvadari/Documents/xbridge_witness/my_build/xbridge_witnessd | /Users/mvadari/Documents/sidechain-config/witness0/witness.json | 127.0.0.1 |       6010
-#  witness1 | 64231 | /Users/mvadari/Documents/xbridge_witness/my_build/xbridge_witnessd | /Users/mvadari/Documents/sidechain-config/witness1/witness.json | 127.0.0.1 |       6011
