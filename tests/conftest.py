08f0a1b7e06c0a9a3ba8e7d99524808ab5ee6023
Mayukha Vadari <mvadari@ripple.com>
2022-09-23 15:32:40 -0400

    only create new folder at beginning of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import shutil
 import tempfile
 import unittest
 import unittest.mock
+from typing import Any, Optional
 
 import pytest
 from click.testing import CliRunner
@@ -11,38 +12,56 @@ from click.testing import CliRunner
 from sidechain_cli.main import main
 from sidechain_cli.utils.config_file import _CONFIG_FILE
 
+tempdir: Optional[tempfile.TemporaryDirectory] = None
+env_vars: Optional[Any] = None
+
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
+    global tempdir, env_vars
+    tempdir = tempfile.TemporaryDirectory()
+    env_vars = unittest.mock.patch.dict(
+        os.environ,
+        {
+            "XCHAIN_CONFIG_DIR": tempdir.name,
+        },
+    )
+    env_vars.start()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """
+    Called after whole test run finished, right before
+    returning the exit status to the system.
+    """
+    shutil.rmtree(tempdir.name)
+    env_vars.stop()
+
 
 @pytest.fixture(autouse=True)
 def runner():
     print("Hiiiiiiiasdufhas8dfoasdijfak")
-    with tempfile.TemporaryDirectory() as tempdir:
-        with unittest.mock.patch.dict(
-            os.environ,
-            {
-                "XCHAIN_CONFIG_DIR": tempdir,
-            },
-        ):
-            # reset CLI config file
-            os.remove(_CONFIG_FILE)
-            with open(_CONFIG_FILE, "w") as f:
-                data = {"chains": [], "witnesses": [], "bridges": []}
-                json.dump(data, f, indent=4)
+    # reset CLI config file
+    os.remove(_CONFIG_FILE)
+    with open(_CONFIG_FILE, "w") as f:
+        data = {"chains": [], "witnesses": [], "bridges": []}
+        json.dump(data, f, indent=4)
 
-            cli_runner = CliRunner()
+    cli_runner = CliRunner()
 
-            # create config files
-            result = cli_runner.invoke(main, ["server", "create-config", "all"])
-            assert result.exit_code == 0
+    # create config files
+    result = cli_runner.invoke(main, ["server", "create-config", "all"])
+    assert result.exit_code == 0
 
-            # start servers
-            start_result = cli_runner.invoke(main, ["server", "start-all", "--verbose"])
-            print(start_result.output)
-            assert start_result.exit_code == 0, start_result.output
+    # start servers
+    start_result = cli_runner.invoke(main, ["server", "start-all", "--verbose"])
+    assert start_result.exit_code == 0, start_result.output
 
-            yield cli_runner
+    yield cli_runner
 
-            # stop servers
-            stop_result = cli_runner.invoke(main, ["server", "stop", "--all"])
-            assert stop_result.exit_code == 0, stop_result.output
-
-        shutil.rmtree(tempdir)
+    # stop servers
+    stop_result = cli_runner.invoke(main, ["server", "stop", "--all"])
+    assert stop_result.exit_code == 0, stop_result.output
