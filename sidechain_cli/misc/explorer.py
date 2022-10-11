809d2ea6353a288c237c3e869b4cc6e7f48bc7fa
Mayukha Vadari <mvadari@ripple.com>
2022-10-10 23:00:15 -0400

    refactor

--- sidechain_cli/misc/explorer.py
+++ sidechain_cli/misc/explorer.py
@@ -5,15 +5,16 @@ import webbrowser
 
 import click
 
+_EXPLORER_HTML_PATH = os.path.abspath(
+    os.path.join(
+        os.path.realpath(__file__),
+        "..",
+        "explorer.html",
+    )
+)
+
 
 @click.command(name="explorer")
 def launch_explorer() -> None:
     """Launch an Explorer connected to your nodes."""
-    filepath = os.path.abspath(
-        os.path.join(
-            os.path.realpath(__file__),
-            "..",
-            "explorer.html",
-        )
-    )
-    webbrowser.open(f"file://{filepath}")
+    webbrowser.open(f"file://{_EXPLORER_HTML_PATH}")
