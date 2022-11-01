b2c17307a4853e58722a525952f08fabebd55bb1
Mayukha Vadari <mvadari@ripple.com>
2022-11-01 16:48:35 -0400

    add multiple account test

--- tests/misc/test_fund.py
+++ tests/misc/test_fund.py
@@ -22,3 +22,20 @@ class TestFund:
         final_account_info = client.request(AccountInfo(account=test_account))
         assert final_account_info.status.value == "success"
         assert final_account_info.result["account_data"]["Account"] == test_account
+
+    def test_fund_multiple_accounts(self, runner):
+        client = get_config().get_chain("locking_chain").get_client()
+
+        test_accounts = [Wallet.create().classic_address for _ in range(4)]
+        for account in test_accounts:
+            initial_account_info = client.request(AccountInfo(account=account))
+            assert initial_account_info.status.value == "error"
+            assert initial_account_info.result["error"] == "actNotFound"
+
+        fund_result = runner.invoke(main, ["fund", "locking_chain", *test_accounts])
+        assert fund_result.exit_code == 0, fund_result.output
+
+        for account in test_accounts:
+            final_account_info = client.request(AccountInfo(account=account))
+            assert final_account_info.status.value == "success"
+            assert final_account_info.result["account_data"]["Account"] == account
