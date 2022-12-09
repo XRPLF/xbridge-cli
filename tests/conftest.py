245d08374c12023f160caa597e42a7d7693b15ce
Mayukha Vadari <mvadari@ripple.com>
2022-12-09 15:01:25 -0500

    add more debug info

--- tests/conftest.py
+++ tests/conftest.py
@@ -91,6 +91,9 @@ def _create_config_files() -> None:
     if _is_docker():
         params.append("--docker")
     result = cli_runner.invoke(main, params)
+    import traceback
+
+    traceback.print_exception(*result.exc_info)  # mypy: ignore
     assert result.exit_code == 0, result.output
 
 
